531d9c0584e543dcaed1e7333099c830390a2e9b
Wesley Cheah <65939868+e0026557@users.noreply.github.com>
2023-01-10 08:19:15 +0000

    Set up Redux

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,61 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import {App} from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-    color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-// Functional Component Method
-// annotation for props object (usually an interface)
-// return type annotation (usually JSX.Element)
-// const App = (props: AppProps): JSX.Element => {
-//     return <div>
-//         {props.color}
-//     </div>
-// }
-
-// Method 2 of handling state
-interface AppState {
-    counter: number
-}
-
-// Method 2: Need to include AppState type as second argument
-class App extends React.Component<AppProps> {
-    // Method 1: Override state property defined inside Component
-    state = {
-        counter: 0
-    };
-
-    // Method 2 of handling state
-    // constructor(props: AppProps) {
-    //     super(props);
-
-    //     this.state = {
-    //         counter: 0
-    //     }
-    // }
-
-    onIncrement = (): void => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-
-    onDecrement = (): void => {
-        this.setState({
-            counter: this.state.counter - 1
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+    , document.querySelector('#root'));
