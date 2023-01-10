f008ae04f31a3ffcaeef7326430e5a5a28c736c4
Wesley Cheah <65939868+e0026557@users.noreply.github.com>
2023-01-10 09:00:39 +0000

    Added validation for redux store state

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
 
-export const reducers = combineReducers({
-    counter: () => 1
+// Describe the entire state in the redux store
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer, // TS is validating that our todosReducer is actually returning an array of todos
 });
